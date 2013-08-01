fa34a2f6f4fd7942c1cb05ad7bf737663864dcdb
Chris Grantham <nospam@fakedomain.fake>
2013-08-01 15:15:56 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'f2677a87db688593a8a15350939879e6af649bd457769e010d6150cc002997f644ffd5a6bcc110cefe642d71016f85d371b4b790aba35be06ce0248c441008cc'
